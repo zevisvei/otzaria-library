9ce0c8a8b9d728d550b03012e5c67247607a8789
zevisvei <101482799+zevisvei@users.noreply.github.com>
2024-12-16 04:50:19 +0200

    Update github.py

--- github.py
+++ github.py
@@ -8,14 +8,14 @@ def sync_files(folder_path, target_folder_path):
             file_path = os.path.join(root, file)
             rel_file_path = os.path.relpath(file_path, folder_path)
             target_file_path = os.path.join(target_folder_path, rel_file_path)
-            target_folder_path = os.path.split(target_file_path)[0]
-            os.makedirs(target_folder_path, exist_ok=True)
+            sync_folder = os.path.split(target_file_path)[0]
+            os.makedirs(sync_folder, exist_ok=True)
             shutil.copy(file_path, target_file_path)
 
 def sync_folders(folder_path, folders_to_update):
     for root, folders, _ in os.walk(folder_path):
         for folder in folders:
-            full_folder_path = os.path.join(folder, root)
+            full_folder_path = os.path.join(root, folder)
             rel_folder_path = os.path.relpath(full_folder_path, folder_path)
             for folder_to_update in folders_to_update:
                 full_target_folder_path = os.path.join(folder_to_update, rel_folder_path)
@@ -39,5 +39,5 @@ for folder in folders:
 folders_updated = sys.argv[1:]
 
 for folder in folders_updated:
-    folders_to_update = (folder for folder in folders if folder not in folders_updated)
+    folders_to_update = [folder for folder in folders if folder not in folders_updated]
     sync_folders(folder, folders_to_update)
