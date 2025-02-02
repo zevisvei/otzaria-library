193bc85d4d867bee5da6b284cd8b3d69bb713ce9
zevisvei <0583238037h@gmail.com>
2025-02-02 03:21:24 +0200

    fix ksk

--- KSK/fix and split/fix.py
+++ KSK/fix and split/fix.py
@@ -9,7 +9,8 @@ import os
 def h_2(text: str):
     if "דף" not in text:
         text = f"דף {text}"
-    text = text.replace("''", '').replace('"', '').replace("עא", ".").replace("עב", ":").strip()
+    text = text.strip("[]. ")
+    text = text.replace("''", '').replace('"', '').replace("'", "").replace(" עא", ".").replace(" עב", ":").strip().replace("דף .", "דף עא").replace("דף :", "דף עב")
     return f"<h2>{text}</h2>"
 
 
@@ -26,7 +27,7 @@ for root, _, files in os.walk(base_folder):
         with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read()
         all_headings = re.findall(r"<h3>(.+?)</h3>", content)
-        all_headings = list(map(lambda x: x.replace("''", '"').strip().strip("][").strip(), all_headings))
+        all_headings = list(map(lambda x: x.replace("''", '"').strip("[]. "), all_headings))
         all_lines = content.split("\n")
         new_list = [*all_lines[0:2], h_3(all_headings[0])]
         index = 0
@@ -42,5 +43,6 @@ for root, _, files in os.walk(base_folder):
         new = "\n".join(new_list)
         new = re.sub(r"<h2>(.+)</h2>", lambda match: h_2(match.group(1)), new)
         new = new.replace("''", '"')
+        new = re.sub(r"[ ]{2,}", " ", new)
         with open(file_path, 'w', encoding='utf-8') as f:
             f.write(new)
