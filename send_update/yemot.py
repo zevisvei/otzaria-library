4d2088e77e9e68c03b263a9b6ab685efe6e16ac9
zevisvei <0583238037h@gmail.com>
2025-02-16 13:10:14 +0200

    split yemot

--- send_update/yemot.py
+++ send_update/yemot.py
@@ -9,7 +9,7 @@ def split_content(content: str) -> list[str]:
     start = 0
     chunk_size = 2000
     while len(content) - start > chunk_size:
-        part = content[start:content.rfind(" ", start, start + chunk_size)]
+        part = content[start:content.rfind("\n", start, start + chunk_size)]
         all_partes.append(part.strip())
         start += len(part)
     all_partes.append(content[start:].strip())
