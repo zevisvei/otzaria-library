fded2c1cf6cbf9899c3364e61799e6bb09df674b
zevisvei <0583238037h@gmail.com>
2025-07-07 14:36:37 +0300

    Refactor sync_offline.py: modularize functions for manifest handling and file operations

--- סקריפטים שונות/sync_offline.py
+++ סקריפטים שונות/sync_offline.py
@@ -1,5 +1,6 @@
 import json
 import os
+import shutil
 
 import requests
 
@@ -7,10 +8,31 @@ import requests
 BASE_URL = "https://raw.githubusercontent.com/zevisvei/otzaria-library/refs/heads/main/"
 MANIFEST_FILE_NAME = "files_manifest.json"
 BASE_PATH = "אוצריא"
+LOCAL_PATH = ""
 DEL_LIST_FILE_NAME = "del_list.txt"
 
 
-def main():
+def copy_manifest() -> None:
+    os.makedirs(BASE_PATH, exist_ok=True)
+    shutil.copy(os.path.join(LOCAL_PATH, MANIFEST_FILE_NAME), os.path.join(BASE_PATH, MANIFEST_FILE_NAME))
+
+
+def copy_files() -> None:
+    shutil.copytree(BASE_PATH, LOCAL_PATH, dirs_exist_ok=True, ignore=lambda _, files: [DEL_LIST_FILE_NAME] if DEL_LIST_FILE_NAME in files else [])
+
+
+def remove_files() -> None:
+    del_list_file_path = os.path.join(BASE_PATH, DEL_LIST_FILE_NAME)
+    with open(del_list_file_path, "r", encoding="utf-8") as f:
+        content = f.readlines()
+    for file_path in content:
+        full_path = os.path.join(LOCAL_PATH, file_path)
+        if os.path.exists(full_path):
+            os.remove(full_path)
+    os.remove(del_list_file_path)
+
+
+def download_new() -> None:
     del_list = []
     new_manifest_url = f"{BASE_URL}/{MANIFEST_FILE_NAME}"
     old_manifest_file_path = os.path.join(BASE_PATH, MANIFEST_FILE_NAME)
@@ -36,7 +58,7 @@ def main():
 
     for book_name in old_manifest_content:
         if book_name not in new_manifest_content:
-            del_list.append(os.path.join(BASE_PATH, book_name.replace("/", os.sep)))
+            del_list.append(book_name.replace("/", os.sep))
 
     with open(old_manifest_file_path, "w", encoding="utf-8") as f:
         json.dump(new_manifest_content, f, indent=2, ensure_ascii=False)
@@ -46,4 +68,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    download_new()
